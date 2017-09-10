98289f7a8129778457fc7749a69fe9118ee4ed9c
Alena Golovina <alenasgolovina@gmail.com>
2017-09-10 12:41:06 -0700

    add custom fonts

--- app/src/main/java/com/epicodus/my_hikes/HikesActivity.java
+++ app/src/main/java/com/epicodus/my_hikes/HikesActivity.java
@@ -1,13 +1,20 @@
 package com.epicodus.my_hikes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HikesActivity extends AppCompatActivity {
+    private TextView mLocationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hikes);
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Here are all the hikes near: " + location);
     }
 }
--- app/src/main/java/com/epicodus/my_hikes/MainActivity.java
+++ app/src/main/java/com/epicodus/my_hikes/MainActivity.java
@@ -1,25 +1,39 @@
 package com.epicodus.my_hikes;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindHikesButton;
+    private EditText mLocationEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindHikesButton = (Button) findViewById(R.id.findHikesButton);
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface amaticboldFont = Typeface.createFromAsset(getAssets(), "fonts/amaticbold.ttf");
+        mAppNameTextView.setTypeface(amaticboldFont);
+
         mFindHikesButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Toast.makeText(MainActivity.this, "Hello Alena!", Toast.LENGTH_LONG).show();
-        }
-    });
+            @Override
+            public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, HikesActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+        });
 }
 }
