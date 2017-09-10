fbe14175d1b03f1fcfa549d0e8e7c0ea99d061a6
Alena Golovina <alenasgolovina@gmail.com>
2017-09-10 11:54:15 -0700

    add button and toast to main activity

---
+++ app/src/main/java/com/epicodus/my_hikes/MainActivity.java
@@ -0,0 +1,25 @@
+package com.epicodus.my_hikes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mFindHikesButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFindHikesButton = (Button) findViewById(R.id.findHikesButton);
+        mFindHikesButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "Hello Alena!", Toast.LENGTH_LONG).show();
+        }
+    });
+}
+}
