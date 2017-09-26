de83b4b9350ad486e1d6990f69eaa87215406f50
Alena Golovina <alenasgolovina@gmail.com>
2017-09-26 10:53:04 -0700

    add activities and layout for user authentication

---
+++ app/src/main/java/com/epicodus/my_hikes/CreateAccountActivity.java
@@ -0,0 +1,13 @@
+package com.epicodus.my_hikes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
